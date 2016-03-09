f9403548975b7251c44a2c0a478b5108d74a7bad
Gabriel Takashi Katakura <gt.katakura@gmail.com>
2016-03-08 21:20:55 -0300

    [FIX] removido "métodos para sincronismo" dos repositórios/serviços
    - Disponibilizar os métodos síncronos para cada ação acaba criando uma
    duplicação de código no desenvolvimento, na manutenção e nos testes
    também terão que ser duplicados.
    
    - Além da duplicação, a atualização de um código pode ser feita somente
    em um método, então um método síncrono pode funcionar enquanto um
    assíncrono pode estar com defeito. Se REALMENTE (bem difícil) for
    necessário o uso de métodos síncronos, crie métodos wrappers, onde o
    método síncrono simplesmente chama o método assíncrono, aplicando o
    sincronismo, mas assim garantindo o comportamento igual entre as
    operações.
    EXEMPLO:
    public async Task<TEntity> GetByIdAsync(long id)
    {
    return await _repository.GetByIdAsync(id);
    }
    
    public TEntity GetById(long id)
    {
    return this.GetByIdAsync(id).Result;
    }
    
    - Além de duplicar código, naturalmente que se adota um padrão por
    utilização dos métodos. Não vejo vantagem no uso dos métodos síncronos e
    assíncronos da mesma ação. Use um ou outro, e nesse caso, o
    assincronismo quase sempre é a melhor opção para operações de I/O.
    
    - Um artigo referente ao assunto:
    - Should I expose synchronous wrappers for asynchronous methods?
    http://blogs.msdn.com/b/pfxteam/archive/2012/04/13/10293638.aspx

--- DotNetTemplate.Domain/Repositories/Interfaces/IRepositoryBase.cs
+++ DotNetTemplate.Domain/Repositories/Interfaces/IRepositoryBase.cs
@@ -8,28 +8,21 @@ namespace DotNetTemplate.Domain.Repositories.Interfaces
 {
     public interface IRepositoryBase<TEntity> where TEntity : BaseEntity
     {
-        void Add(TEntity obj);
         Task AddAsync(TEntity obj);
 
-        void Update(TEntity obj);
         Task UpdateAsync(TEntity obj);
-
-        void Remove(TEntity obj);
+        
         Task RemoveAsync(TEntity obj);
 
-        TEntity GetById(long id);
         Task<TEntity> GetByIdAsync(long id);
 
-        TEntity GetById(Expression<Func<TEntity, bool>> predicate, params string[] includes);
         Task<TEntity> GetByIdAsync(Expression<Func<TEntity, bool>> predicate, params string[] includes);
-
-        IEnumerable<TEntity> GetAll(params string[] includes);
+        
         Task<IEnumerable<TEntity>> GetAllAsync(params string[] includes);
 
-        IEnumerable<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate = null, Expression<Func<TEntity, object>> order = null,
-            bool reverse = false, int skipRecords = 0, int takeRecords = 0, params string[] includes);
         Task<IEnumerable<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> predicate = null, Expression<Func<TEntity, object>> order = null,
                     bool reverse = false, int skipRecords = 0, int takeRecords = 0, params string[] includes);
+
         IEnumerable<TEntity> GetAll(ref int totalRecords, Expression<Func<TEntity, bool>> predicate = null, Expression<Func<TEntity, object>> order = null,
             bool reverse = false, int skipRecords = 0, int takeRecords = 0, params string[] includes);
     }
--- DotNetTemplate.Infrastructure.Database/Repositories/RepositoryBase.cs
+++ DotNetTemplate.Infrastructure.Database/Repositories/RepositoryBase.cs
@@ -20,23 +20,10 @@ namespace DotNetTemplate.Infrastructure.Database.Repositories
             Connection = context;
         }
 
-        public virtual void Add(TEntity obj)
-        {
-            Connection.Set<TEntity>().Add(obj);
-            Connection.SaveChanges();
-        }
-
         public async virtual Task AddAsync(TEntity obj)
         {
             Connection.Set<TEntity>().Add(obj);
             await Connection.SaveChangesAsync();
-        }
-
-        public virtual void Update(TEntity obj)
-        {
-            obj.UpdatedAt = DateTime.Now;
-            Connection.Entry(obj).State = EntityState.Modified;
-            Connection.SaveChanges();
         }
 
         public async virtual Task UpdateAsync(TEntity obj)
@@ -46,13 +33,6 @@ namespace DotNetTemplate.Infrastructure.Database.Repositories
             await Connection.SaveChangesAsync();
         }
 
-        public virtual void Remove(TEntity obj)
-        {
-            Connection.Entry(obj).State = EntityState.Deleted;
-            Connection.Set<TEntity>().Remove(obj);
-            Connection.SaveChanges();
-        }
-
         public async virtual Task RemoveAsync(TEntity obj)
         {
             Connection.Entry(obj).State = EntityState.Deleted;
@@ -60,22 +40,9 @@ namespace DotNetTemplate.Infrastructure.Database.Repositories
             await Connection.SaveChangesAsync();
         }
 
-        public TEntity GetById(long id)
-        {
-            return Connection.Set<TEntity>().Find(id);
-        }
-
         public async Task<TEntity> GetByIdAsync(long id)
         {
             return await Connection.Set<TEntity>().FindAsync(id);
-        }
-
-        public TEntity GetById(Expression<Func<TEntity, bool>> predicate, params string[] includes)
-        {
-            IQueryable<TEntity> query = Connection.Set<TEntity>();
-            if (includes != null && includes.Length > 0)
-                Array.ForEach<String>(includes, x => { query = query.Include(x); });
-            return query.FirstOrDefault(predicate);
         }
 
         public async Task<TEntity> GetByIdAsync(Expression<Func<TEntity, bool>> predicate, params string[] includes)
@@ -86,33 +53,12 @@ namespace DotNetTemplate.Infrastructure.Database.Repositories
             return await query.FirstOrDefaultAsync(predicate);
         }
 
-        public virtual IEnumerable<TEntity> GetAll(params string[] includes)
-        {
-            IQueryable<TEntity> query = Connection.Set<TEntity>();
-            if (includes != null && includes.Length > 0)
-                Array.ForEach<String>(includes, x => { query = query.Include(x); });
-            return query.ToList();
-        }
-
         public async virtual Task<IEnumerable<TEntity>> GetAllAsync(params string[] includes)
         {
             IQueryable<TEntity> query = Connection.Set<TEntity>();
             if (includes != null && includes.Length > 0)
                 Array.ForEach<String>(includes, x => { query = query.Include(x); });
             return await query.ToListAsync();
-        }
-
-        public IEnumerable<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate = null, Expression<Func<TEntity, object>> order = null, bool reverse = false, int skipRecords = 0, int takeRecords = 0, params string[] includes)
-        {
-            IQueryable<TEntity> query = Connection.Set<TEntity>();
-            if (includes != null && includes.Length > 0)
-                Array.ForEach<String>(includes, x => { query = query.Include(x); });
-            query = query.Where(predicate ?? (x => true));
-            query = reverse ? order == null ? query.OrderBy(x => x.Id) : query.OrderBy(order) :
-                              order == null ? query.OrderByDescending(x => x.Id) : query.OrderByDescending(order);
-
-            return (skipRecords == 0 && takeRecords == 0 ? query :
-                query.Skip(skipRecords).Take(takeRecords)).ToList();
         }
 
         public async Task<IEnumerable<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> predicate = null, Expression<Func<TEntity, object>> order = null, bool reverse = false, int skipRecords = 0, int takeRecords = 0, params string[] includes)
